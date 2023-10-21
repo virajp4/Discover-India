52c766627bb03cf95e4f486cb5b8e95928464c20
diclub-srmvdp <viraj.patel482@gmail.com>
2023-10-22 04:11:26 +0530

    final backend changes

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
 const User = require('./models/user');
 
@@ -20,7 +22,10 @@ const userRoutes = require('./routes/users')
 const reviewRoutes = require('./routes/reviews');
 const spotRoutes = require('./routes/spots');
 
-mongoose.connect('mongodb://127.0.0.1:27017/spotsDB', {
+// const DB_URL = process.env.DB_URL
+const DB_URL = 'mongodb://localhost:27017/spotsDB'
+
+mongoose.connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -41,12 +46,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const store = MongoStore.create({
+    mongoUrl: DB_URL,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'virajp4'
+    }
+});
+
+store.on('error', function (e) {
+    console.log('Session Store Error', e);
+});
+
 const sessionConfig = {
-    secret: 'viraj',
+    store,
+    name: 'session',
+    secret: 'virajp4',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
         maxAge: (1000 * 60 * 60 * 24 * 7)
     }
@@ -62,6 +82,52 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(mongoSanitize({ replaceWith: '_' }));
+
+app.use(helmet());
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dyw13siqk/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
